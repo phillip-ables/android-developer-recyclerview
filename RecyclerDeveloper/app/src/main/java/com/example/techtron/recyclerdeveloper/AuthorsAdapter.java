1e528a2c6019c19d9764b1ac416b1476e3a47494
Phillip <phillipables1@gmail.com>
2018-02-17 00:05:33 -0500

    store member variable and context return

--- RecyclerDeveloper/app/src/main/java/com/example/techtron/recyclerdeveloper/AuthorsAdapter.java
+++ RecyclerDeveloper/app/src/main/java/com/example/techtron/recyclerdeveloper/AuthorsAdapter.java
@@ -1,10 +1,14 @@
 package com.example.techtron.recyclerdeveloper;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.List;
+
 public class AuthorsAdapter extends RecyclerView.Adapter<AuthorsAdapter.ViewHolder>{
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -29,5 +33,15 @@ public class AuthorsAdapter extends RecyclerView.Adapter<AuthorsAdapter.ViewHold
             nameTextView = (TextView) itemView.findViewById(R.id.author_name);
             readButton = (Button) itemView.findViewById(R.id.read_author);
         }
+    }
+    private List<AuthorsList>  mAuthors;
+    private Context mContext;
+
+    public AuthorsAdapter(Context context, List<AuthorsList> authors){
+        mContext = context;
+        mAuthors = authors;
+    }
+    private Context getContext(){
+        return mContext;
     }
 }
