4bab7b87433dc5521c59b2d2cc787438b528f681
Phillip <phillipables1@gmail.com>
2018-03-28 16:06:36 -0400

    declare contacts List and Context variables for easy access then on ContactsAdapter constructs the context and contacts, private method getContext that returns context

--- app/src/main/java/com/example/techtron/developer_recyclerview/ContactsAdapter.java
+++ app/src/main/java/com/example/techtron/developer_recyclerview/ContactsAdapter.java
@@ -1,14 +1,16 @@
 package com.example.techtron.developer_recyclerview;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class ContactsAdapter extends
     RecyclerView.Adapter<ContactsAdapter.ViewHolder>{
-
     public class ViewHolder extends RecyclerView.ViewHolder {
         public TextView nameTextView;
         public Button messageButton;
@@ -19,5 +21,32 @@ public class ContactsAdapter extends
             nameTextView = (TextView) itemView.findViewById(R.id.tv_contactName);
             messageButton = (Button) itemView.findViewById(R.id.btn_message);
         }
+    }
+
+    private List<Contact> mContacts;
+    private Context mContext;
+
+    public ContactsAdapter (Context context, List<Contact> contacts) {
+        mContacts = contacts;
+        mContext = context;
+    }
+
+    private Context getContext() {
+        return mContext;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
     }
 }
