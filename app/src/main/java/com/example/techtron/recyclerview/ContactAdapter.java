52260e03c466c9caf92a0fd30315e988ab6f6bfc
Phillip <phillipables1@gmail.com>
2018-03-28 19:57:08 -0400

    java class ContactAdapter extends RecyclerView.Adapter(ContactAdapter.ViewHolder) then set up a public ViewHolder extending RecyclerView.ViewHolder method

---
+++ app/src/main/java/com/example/techtron/recyclerview/ContactAdapter.java
@@ -0,0 +1,11 @@
+package com.example.techtron.recyclerview;
+
+import android.support.v7.widget.RecyclerView;
+
+public class ContactAdapter
+    extends RecyclerView.Adapter(ContactAdapter.ViewHolder){
+
+    public ViewHolder extends RecyclerView.ViewHolder{
+
+        }
+}
