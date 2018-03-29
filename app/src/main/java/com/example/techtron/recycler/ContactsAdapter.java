2aa7582512168bb82dbe3b193f0987db11f039db
Phillip <phillipables1@gmail.com>
2018-03-29 10:38:23 -0400

    onCreateViewHolder get Parent context for context and instatiate LayoutInflater from context

--- app/src/main/java/com/example/techtron/recycler/ContactsAdapter.java
+++ app/src/main/java/com/example/techtron/recycler/ContactsAdapter.java
@@ -2,9 +2,12 @@ package com.example.techtron.recycler;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -24,26 +27,28 @@ public class ContactsAdapter
             uName = (TextView) itemView.findViewById(R.id.nameTV);
             message = (Button) itemView.findViewById(R.id.messageBTN);
         }
-
-        //this is where it gets blurry, but i halfway knew it
-        //declare variables set in constructor and make get context method
-        List<Contacts> mContacts;
-        Context mContext;
-
-        public ContactsAdapter(List<Contacts> contacts, Context context){
-            mContacts = contacts;
-            mContext = context;
-        }
-
-        public Context getContext(){
-            return mContext;
-        }
     }
 
+    //this is where it gets blurry, but i halfway knew it
+    //declare variables set in constructor and make get context method
+    List<Contacts> mContacts;
+    Context mContext;
 
+    public ContactsAdapter(List<Contacts> contacts, Context context){
+        mContacts = contacts;
+        mContext = context;
+    }
+
+    public Context getContext(){
+        return mContext;
+    }
 
     @Override
+    //think about what these parameters are for and why you need them
     public ContactsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
         return null;
     }
 
