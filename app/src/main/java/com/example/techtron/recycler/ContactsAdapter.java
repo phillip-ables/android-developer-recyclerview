c501fd34a4d3211e64615d870fecb0031e0feb59
Phillip <phillipables1@gmail.com>
2018-03-29 08:46:33 -0400

    java ContactsAdapter that extends recyclerView.Adapter with methods implemented

---
+++ app/src/main/java/com/example/techtron/recycler/ContactsAdapter.java
@@ -0,0 +1,22 @@
+package com.example.techtron.recycler;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class ContactsAdapter
+    extends RecyclerView.Adapter<ContactsAdapter.ViewHolder>{
+    @Override
+    public ContactsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ContactsAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
