eb909bf4d9455330d81f9b5ffcee212ede8a2dcb
Phillip <phillipables1@gmail.com>
2018-03-29 08:54:26 -0400

    ViewHolder class that constructs a ViewHolder extended from RecyclerView.ViewHolder and constructs a super from View and finds TextView and Button from user row

--- app/src/main/java/com/example/techtron/recycler/ContactsAdapter.java
+++ app/src/main/java/com/example/techtron/recycler/ContactsAdapter.java
@@ -1,10 +1,34 @@
 package com.example.techtron.recycler;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ContactsAdapter
     extends RecyclerView.Adapter<ContactsAdapter.ViewHolder>{
+
+    //remember you are creating a custom view holder, just setting it up is all, that simple
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView uName;
+        public Button message;
+
+        public ViewHolder(View itemView){
+            super (itemView);
+
+            uName = (TextView) itemView.findViewById(R.id.nameTV);
+            message = (Button) itemView.findViewById(R.id.messageBTN);
+        }
+
+        //this is where it gets blurry, but i halfway knew it
+
+    }
+
+
+
     @Override
     public ContactsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         return null;
